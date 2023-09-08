ed336d2889c3f0a6077fbe3c1639d67e504fe517
pawelczuban <80459075+pawelczuban@users.noreply.github.com>
2023-09-08 02:20:01 +0200

    final

---
+++ ManageMe/src/app/dashboard/dashboard-content/project-details.ts
@@ -0,0 +1,53 @@
+export const projectDetails = [
+    {
+        id: 1,
+        title: 'P1',
+        description: 'P1 WSEI ',
+        functionallities: [
+            {
+                functionallityID: 1,
+                functionallityName: 'sth1',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'sth2',
+                        status: 'ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 2,
+                functionallityName: 'sth3',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'sth4',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 3,
+                functionallityName: 'sth5',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'sth6',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 4,
+                functionallityName: 'sth7',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'sth8',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ ManageMe/src/app/dashboard/dashboard-navbar/dashboard-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-navbar',
+  templateUrl: './dashboard-navbar.component.html',
+  styleUrls: ['./dashboard-navbar.component.scss']
+})
+
+export class DashboardNavbarComponent {
+
+}
